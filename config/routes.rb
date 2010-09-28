c7a7e9a65e21711d5dd7680ec8f028dab2042b8e
Denis Lifanov <inadsence@gmail.com>
2010-09-28 23:51:47 +0400

    root to questions#search

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,8 @@ Questionnaire::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
+  root :to => 'questions#search'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
