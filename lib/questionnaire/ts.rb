9718fe09df52061c9610749d52a7102bb2d82d9e
Denis Lifanov <inadsence@gmail.com>
2010-09-30 12:47:15 +0400

    monkeypatched ThinkingSphinx::Search#excerpt_for (it should accepts some options like :limit etc)

---
+++ lib/questionnaire/ts.rb
@@ -0,0 +1,18 @@
+module ThinkingSphinx
+  class Search
+    def excerpt_for(*args)
+      opts, string, model = args.extract_options!, *args
+
+      if model.nil? && one_class
+        model ||= one_class
+      end
+
+      populate
+      client.excerpts(opts.update(
+        :docs   => [string],
+        :words  => results[:words].keys.join(' '),
+        :index  => options[:index] || "#{model.source_of_sphinx_index.sphinx_name}_core"
+      )).first
+    end
+  end
+end
